5a81b5d339c8269791493669939d45b873937331
MajharolTanvir <majharul5456@gmail.com>
2024-06-02 14:49:04 +0600

    App.css file setup with css elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
 
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header } from './containers'
+import { CTA, Brand, Navbar } from './components'
+
 function App() {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
